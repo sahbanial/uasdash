7734973520e1d7e20c7d8b6b8b197d08450cb245
Houadria Hamza <hhouadria@toolynk.com>
2020-10-26 21:53:46 +0100

    Updates

---
+++ precache-manifest.057720d8971212da1512f8fa446226d7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/uasdash/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "484f530f7fdead81acf7",
+    "url": "/uasdash/static/css/main.bc5f2e8d.chunk.css"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/uasdash/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "950ad25752b8ac9f4086",
+    "url": "/uasdash/static/js/1.950ad257.chunk.js"
+  },
+  {
+    "revision": "1d45410aee0c5b24140f",
+    "url": "/uasdash/static/js/runtime~main.1d45410a.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/uasdash/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/uasdash/static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/uasdash/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/uasdash/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "484f530f7fdead81acf7",
+    "url": "/uasdash/static/js/main.484f530f.chunk.js"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/uasdash/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/uasdash/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/uasdash/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/uasdash/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/uasdash/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/uasdash/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "950ad25752b8ac9f4086",
+    "url": "/uasdash/static/css/1.bebf5f9a.chunk.css"
+  },
+  {
+    "revision": "0be58be37c24fd8be5e6bfc325dfbd85",
+    "url": "/uasdash/index.html"
+  }
+];
